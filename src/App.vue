8dda4644f6961a458cf6dfec7922488361f3213e
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-11-24 23:18:14 +0500

    project init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Sort />
+  </div>
+</template>
+
+<script>
+
+import Sort from '@/components/with-paging';
+export default {
+  name: 'App',
+  components: {
+    Sort
+  }
+};
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
